70f39ff348de088d79bbb0b81673f33fdb147654
elsalvacion <elsalvacion2022@gmail.com>
2021-08-13 03:43:12 +0800

    sample projects and sidenav section added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const toTop = document.querySelector('.to-top')
-
+const closeBtn = document.querySelector('#closeBtn');
+const sidenavContainer = document.querySelector('#sidenavContainer');
+const sidenavContent = document.querySelector('#sidenavContent');
+const toggle = document.querySelector('.toggle')
 document.addEventListener('DOMContentLoaded', () => {
     
     window.addEventListener('scroll', showArrow);
+    closeBtn.addEventListener('click', closeOnClick)
+    sidenavContainer.addEventListener('click', closeOnClick)
+    toggle.addEventListener('click', toggleShow)
 });
 
 function showArrow(e) {
@@ -16,3 +22,15 @@ function showArrow(e) {
 
     }
 }
+
+function toggleShow(e) {
+    sidenavContainer.classList.toggle('show')
+}
+
+function closeOnClick(e){
+
+    if(!e.target.classList.contains('not-a-closer')) {
+        sidenavContainer.classList.remove('show')
+    }
+}
+
