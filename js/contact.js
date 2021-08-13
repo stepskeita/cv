f448b33c649ba9631bdcabe1040359b525dfeedd
elsalvacion <elsalvacion2022@gmail.com>
2021-08-14 01:51:48 +0800

    contact complete

--- js/contact.js
+++ js/contact.js
@@ -3,20 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const name = document.querySelector('#name');
     const email = document.querySelector('#email')
     ;
-    const message = document.querySelector('#message')
+    const message = document.querySelector('#message');
+    const section = document.querySelector('section')
+    const alert = document.querySelector('.alert')
+    const spinner =  `<div class="lds-facebook"><div></div><div></div><div></div></div>`
 
-    form.addEventListener('submit', async (e) => {
+
+    form.addEventListener('submit', sendData);
+
+    async function sendData (e){
+
+        section.innerHTML = spinner;
+
         e.preventDefault();
+
         const data = {
             name: name.value,
             email: email.value,
             message: message.value,
         }
-
-        console.log(JSON.stringify(data))
         
-        const url = "https://alieu-cv-backend.herokuapp.com/";
-
+        const url = "https://alieu-cv-backend.herokuapp.com";
+    
         const res = await fetch(url,  {
             method: "POST",
             headers: {
@@ -26,7 +34,30 @@ document.addEventListener('DOMContentLoaded', () => {
             body: JSON.stringify(data),
         });
 
-        const json = await res.json();
-        console.log(json);
-    })
+        const status = await res.status;
+        section.innerHTML = ``
+        section.appendChild(form)
+        if(status !== 200) {
+            alert.innerHTML = `Server error send me a message on alieukeita201@gmail.com`
+            alert.classList.add('error')
+        }
+        else {
+            alert.innerHTML = `Message sent`
+            alert.classList.add('success')
+        }
+        
+       setTimeout(() => {
+        if(alert.classList.contains('error')) {
+            alert.classList.remove('error')
+        }
+        else if (alert.classList.contains('success')) {
+            alert.classList.remove('success')
+
+        }
+       }, 10000)
+
+        
+        
+    }
 })
+
